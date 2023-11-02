cbe68a0886f9caf39f599a5e5bdce100e0b46362
bryce <50299051+bryce-seifert@users.noreply.github.com>
2023-11-01 21:40:49 -0500

    initial release (#1)

--- main.js
+++ main.js
@@ -1,63 +1,197 @@
-const { InstanceBase, Regex, runEntrypoint, InstanceStatus } = require('@companion-module/base')
-const UpgradeScripts = require('./upgrades')
-const UpdateActions = require('./actions')
-const UpdateFeedbacks = require('./feedbacks')
-const UpdateVariableDefinitions = require('./variables')
+import { InstanceBase, Regex, runEntrypoint, InstanceStatus } from '@companion-module/base'
+import { getActions } from './actions.js'
+import { getPresets } from './presets.js'
+import { getVariables } from './variables.js'
+import { getFeedbacks } from './feedbacks.js'
+import UpgradeScripts from './upgrades.js'
 
-class ModuleInstance extends InstanceBase {
+import OSC from 'osc'
+
+class FarragoInstance extends InstanceBase {
 	constructor(internal) {
 		super(internal)
 	}
 
 	async init(config) {
 		this.config = config
+		this.updateStatus(InstanceStatus.Connecting)
 
-		this.updateStatus(InstanceStatus.Ok)
+		this.initOSC()
 
-		this.updateActions() // export actions
-		this.updateFeedbacks() // export feedbacks
-		this.updateVariableDefinitions() // export variable definitions
+		this.initActions()
+		this.initFeedbacks()
+		this.initVariables()
+		this.initPresets()
 	}
-	// When module gets deleted
+
 	async destroy() {
+		if (this.listener) {
+			this.listener.close()
+		}
+
 		this.log('debug', 'destroy')
 	}
 
 	async configUpdated(config) {
 		this.config = config
+		this.initOSC()
 	}
 
-	// Return config fields for web config
 	getConfigFields() {
 		return [
 			{
 				type: 'textinput',
 				id: 'host',
-				label: 'Target IP',
-				width: 8,
+				label: 'IP Address',
+				width: 4,
 				regex: Regex.IP,
 			},
 			{
 				type: 'textinput',
 				id: 'port',
-				label: 'Target Port',
+				label: 'Farrago Input Port',
 				width: 4,
+				default: '8090',
+				regex: Regex.PORT,
+			},
+			{
+				type: 'textinput',
+				id: 'feedbackPort',
+				label: 'Farrago Output Port',
+				width: 4,
+				default: '8091',
 				regex: Regex.PORT,
 			},
 		]
 	}
 
-	updateActions() {
-		UpdateActions(this)
+	initVariables() {
+		const variables = getVariables.bind(this)()
+		this.setVariableDefinitions(variables)
 	}
 
-	updateFeedbacks() {
-		UpdateFeedbacks(this)
+	initFeedbacks() {
+		const feedbacks = getFeedbacks.bind(this)()
+		this.setFeedbackDefinitions(feedbacks)
 	}
 
-	updateVariableDefinitions() {
-		UpdateVariableDefinitions(this)
+	initPresets() {
+		const presets = getPresets.bind(this)()
+		this.setPresetDefinitions(presets)
+	}
+
+	initActions() {
+		const actions = getActions.bind(this)()
+		this.setActionDefinitions(actions)
+	}
+
+	sendCommand(command, value) {
+		if (value || value === 0) {
+			this.oscSend(this.config.host, this.config.port, `${command}`, [
+				{
+					type: 'i',
+					value: value,
+				},
+			])
+		} else {
+			this.oscSend(this.config.host, this.config.port, `${command}`, [])
+		}
+	}
+
+	initOSC() {
+		this.tiles = []
+
+		if (this.listener) {
+			this.listener.close()
+		}
+
+		this.listener = new OSC.UDPPort({
+			localAddress: '0.0.0.0',
+			localPort: this.config.feedbackPort,
+			broadcast: true,
+			metadata: true,
+		})
+
+		this.listener.open()
+		this.listener.on('ready', () => {
+			this.updateStatus(InstanceStatus.Ok)
+			this.sendCommand('/ping')
+		})
+		this.listener.on('error', (err) => {
+			if (err.code == 'EADDRINUSE') {
+				this.log('error', `Error: Selected feedback port ${err.message.split(':')[1]} is already in use.`)
+				this.updateStatus('bad_config', 'Feedback port conflict')
+			}
+		})
+
+		this.listener.on('message', (message) => {
+			if (message) {
+				let value = message.args[0]?.value
+				let address = message.address
+
+				if (address.match(/\/set\/\d+\/tile\/\d+\/\d+\/[A-Za-z]+$/)) {
+					let info = message.address.match(/(\/set\/\d+\/tile\/\d+\/\d+)\/([A-Za-z]+)$/)
+					let id = info[1]
+					let prop = info[2]
+					let index = this.tiles.findIndex((tile) => tile.id === id)
+					let variableName = id.replaceAll('/', '_').slice(1)
+
+					if (index > -1) {
+						switch (prop) {
+							case 'title':
+								if (this.tiles[index].label !== value) {
+									this.tiles[index].label = value
+									this.updateCompanionInternals()
+									this.setVariableValues({ [`${variableName}_name`]: value })
+								}
+								break
+							case 'play':
+								if (this.tiles[index].play !== value) {
+									this.tiles[index].play = value
+									this.checkFeedbacks('tilePlaying')
+								}
+								break
+							case 'color':
+								if (this.tiles[index].color !== value) {
+									this.tiles[index].color = value
+									this.checkFeedbacks('tileColor')
+								}
+								break
+							case 'icon':
+								if (this.tiles[index].icon !== value) {
+									this.tiles[index].icon = value
+									this.setVariableValues({ [`${variableName}_icon`]: value })
+								}
+								break
+							default:
+							/* if (this.tiles[index][`${prop}`] !== value) {
+									this.tiles[index][`${prop}`] = value
+								} */
+						}
+					} else if (index === -1) {
+						this.tiles.push({
+							id: id,
+							[`${prop}`]: value,
+						})
+					}
+				}
+			}
+		})
+	}
+
+	updateCompanionInternals() {
+		//Sort tiles by set, then x position, then y position
+		this.tiles.sort((a, b) =>
+			a.id.localeCompare(b.id, undefined, {
+				numeric: true,
+			})
+		)
+
+		this.initActions()
+		this.initPresets()
+		this.initFeedbacks()
+		this.initVariables()
 	}
 }
 
-runEntrypoint(ModuleInstance, UpgradeScripts)
+runEntrypoint(FarragoInstance, UpgradeScripts)
